257f31a2514a69ef3601328def9a387682d2f670
goodlikee <goodlikee19941822>
2019-01-17 01:11:23 +0400

    add NuGet-packages and Entities

---
+++ Entities/Cost.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities
+{
+    public class Cost
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Value { get; set; }
+        public DateTime Date { get; set; }
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
