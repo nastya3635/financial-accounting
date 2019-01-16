b7a0d27c8bbba661b3e401cebe29c305ee56cbc8
goodlikee <goodlikee19941822>
2019-01-17 03:22:58 +0400

    17.01.19

---
+++ Context/CostsContext/CostsContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Entities;
+
+namespace test2.Models
+{
+    public class CostsContext : DbContext
+    {
+        public DbSet<Cost> Costs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public CostsContext(DbContextOptions<CostsContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
--- test3/Startup.cs
+++ test3/Startup.cs
@@ -2,19 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNet.OData.Builder;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.OData.Edm;
+using Entities;
+using Microsoft.AspNet.OData.Extensions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Context;
+
 
 namespace test3
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            // получаем строку подключения из файла конфигурации
+            string connection = Configuration.GetConnectionString("DefaultConnection");
+            // добавляем контекст CostsContext в качестве сервиса в приложение
+            services.AddDbContext<CostsContext>(options => options.UseSqlServer(connection));
+            services.AddOData();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +46,26 @@ namespace test3
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
+            app.UseMvc(b =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                b.MapODataServiceRoute("odata", "odata", GetEdmModel());
             });
         }
+
+        private static IEdmModel GetEdmModel()
+        {
+            ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+            builder.EntitySet<Cost>("Costs");
+            builder.EntitySet<Category>("Categories");
+            return builder.GetEdmModel();
+        }
+
+        //private static IEdmModel GetEdmModel()
+        //{
+        //    ODataConventionModelBuilder builder = new ODataConventionModelBuilder();
+        //    builder.EntitySet<Cost>("Costs");
+        //    builder.EntitySet<Category>("Categories");
+        //    return builder.GetEdmModel();
+        //}
     }
 }
